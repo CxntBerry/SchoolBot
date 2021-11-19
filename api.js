c3993223c878d3ad05bc4b6c4a82523a637afa5c
CxntBerry <hoesnt@gmail.com>
2021-11-19 12:20:05 -0700

    so captcha????

--- api.js
+++ api.js
@@ -3,7 +3,16 @@ const app = express();
 
 app.get('/api', (req, res) => {
   const link = req.query.link;
+  const puppeteer = require('puppeteer');
 
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(link);
+    await page.screenshot({ path: 'example.png' });
+
+    await browser.close();
+  })();
   var array = [link, 'exampleAnswer'];
   res.send({ link: array[0],
             answers: array[1] });
