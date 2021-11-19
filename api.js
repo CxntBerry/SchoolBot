f1d7b5f6b79e9043a48c24365c4af641ee6b51a3
CxntBerry <hoesnt@gmail.com>
2021-11-19 16:22:25 -0700

    Added headers to xhr

--- api.js
+++ api.js
@@ -11,22 +11,29 @@ app.get('/api', (req, res) => {
   const link = req.query.link;
 
   var xhr = new XMLHttpRequest();
-
-  xhr.onreadystatechange = function() {
-	   console.log("State: " + this.readyState);
-	    if (this.readyState === 4) {
-		    console.log("Body:\n" + this.responseBody);
-	    }
-  };
+  xhr.responseType = 'json';
   xhr.open("GET", link);
+  xhr.setRequestHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0');
+  xhr.setRequestHeader('Accept', 'text/html');
+  xhr.setRequestHeader('Accept-Language', 'en-US,en;q=0.5');
+  xhr.setRequestHeader('Accept-Encoding', 'gzip, deflate');
+  xhr.setRequestHeader('Upgrade-Insecure-Requests', '1');
+  xhr.setRequestHeader('Sec-Fetch-Dest', 'document');
+  xhr.setRequestHeader('Sec-Fetch-Mode', 'navigate');
+  xhr.setRequestHeader('Sec-Fetch-Site', 'none');
+  xhr.setRequestHeader('Sec-Fetch-User', '?1');
+  xhr.onload = function() {
+	   console.log(xhr.status);
+     var scrapedData = {1:xhr.responseText};
+     var array = [link, scrapedData];
+     res.send({ link: array[0],
+               answers: array[1] });
+	};
   xhr.send();
 
   console.log("New GET request to /api?link=" + link + " on address: " + req.connection.remoteAddress);
 
-  var scrapedData = {1:"test", 2:"ddd"};
-  var array = [link, scrapedData];
-  res.send({ link: array[0],
-            answers: array[1] });
+
 });
 
 
