759f28411ac7f973cdef94484eecdaf766075609
CxntBerry <hoesnt@gmail.com>
2021-11-19 12:17:08 -0700

    Returns data based on link

--- api.js
+++ api.js
@@ -4,8 +4,9 @@ const app = express();
 app.get('/api', (req, res) => {
   const link = req.query.link;
 
-  var array = [link, 'tb', 'abc'];
-  res.send({ data: array });
+  var array = [link, 'exampleAnswer'];
+  res.send({ link: array[0],
+            answers: array[1] });
 });
 
 app.listen(8080, () => console.log('alive on localhost:8080'));
