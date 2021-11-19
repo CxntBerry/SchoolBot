ec0ad596edde50fa02e0d315dddd5dfa7998d60c
CxntBerry <hoesnt@gmail.com>
2021-11-19 16:59:13 -0700

    Added headers

--- api.js
+++ api.js
@@ -13,10 +13,10 @@ app.get('/api', (req, res) => {
   var xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
   xhr.open("GET", link);
+  xhr.withCredentials = false;
   xhr.setRequestHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0');
   xhr.setRequestHeader('Accept', 'text/html');
   xhr.setRequestHeader('Accept-Language', 'en-US,en;q=0.5');
-  xhr.setRequestHeader('Accept-Encoding', 'gzip, deflate');
   xhr.setRequestHeader('Upgrade-Insecure-Requests', '1');
   xhr.setRequestHeader('Sec-Fetch-Dest', 'document');
   xhr.setRequestHeader('Sec-Fetch-Mode', 'navigate');
