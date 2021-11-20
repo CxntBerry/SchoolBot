d4792b6a6bc4200fbede72b22e0121b1395708c7
CxntBerry <hoesnt@gmail.com>
2021-11-19 18:06:42 -0700

    header stuff

--- api.js
+++ api.js
@@ -14,9 +14,12 @@ app.get('/api', (req, res) => {
   xhr.responseType = 'json';
   xhr.open("GET", link);
   xhr.withCredentials = false;
+  xhr.setRequestHeader('Host', 'brainly.com');
   xhr.setRequestHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0');
-  xhr.setRequestHeader('Accept', 'text/html');
+  xhr.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8');
   xhr.setRequestHeader('Accept-Language', 'en-US,en;q=0.5');
+  xhr.setRequestHeader('Connection', 'close');
+  xhr.setRequestHeader('DNT', '1');
   xhr.setRequestHeader('Upgrade-Insecure-Requests', '1');
   xhr.setRequestHeader('Sec-Fetch-Dest', 'document');
   xhr.setRequestHeader('Sec-Fetch-Mode', 'navigate');
